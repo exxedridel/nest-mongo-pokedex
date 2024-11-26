8a73d5bf5d1a1f198311b077e91978d2ef430e47
dev_hrios <hrios@tiprotec.com>
2024-11-26 00:38:49 -0600

    insertar lote de info pero con await (no recomendado)

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios
 
+  // necessario importar el injectModel correspondiente para insertar en la DB
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) { }
+
   async executeSeed() {
     const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
 
-    data.results.forEach(({ name, url }) => {
+    await data.results.forEach(async({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-      console.log({ name, no })
+
+
+      const createPokemonDto = { name, no }
+      // console.log(createPokemonDto)
+
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
     })
 
-    return data.results;
+    return 'Seed Executed';
   }
 
 }
